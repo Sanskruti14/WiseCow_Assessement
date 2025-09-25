365096511003ea16c4ab8480bc6cf11d8263faac
Sanskruti14 <sanubagul2001@gmail.com>
2025-09-26 00:06:53 +0530

    added kubernetes deployment manifest

---
+++ k8s/delete.sh
@@ -0,0 +1,4 @@
+kubectl delete -f ingress.yaml
+kubectl delete -f wisecow-service.yaml
+kubectl delete -f wisecow-deployment.yaml
+
---
+++ k8s/run.sh
@@ -0,0 +1,4 @@
+kubectl apply -f ingress.yaml
+kubectl apply -f wisecow-service.yaml
+kubectl apply -f wisecow-deployment.yaml
+
