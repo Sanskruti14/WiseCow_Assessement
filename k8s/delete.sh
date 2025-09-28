9d5a2a45132122dff9e84f458ed51b1b39cbfb89
Sanskruti14 <sanubagul2001@gmail.com>
2025-09-28 22:08:46 +0530

    added kubearmor and namespace yaml file

--- k8s/delete.sh
+++ k8s/delete.sh
@@ -1,4 +1,5 @@
 kubectl delete -f ingress.yaml
 kubectl delete -f wisecow-service.yaml
 kubectl delete -f wisecow-deployment.yaml
-
+kubectl delete -f karmor.yaml
+kubectl delete -f namespace.yaml
--- k8s/run.sh
+++ k8s/run.sh
@@ -1,3 +1,5 @@
+kubectl apply -f namespace.yaml
+kubectl apply -f karmor.yaml
 kubectl apply -f ingress.yaml
 kubectl apply -f wisecow-service.yaml
 kubectl apply -f wisecow-deployment.yaml
